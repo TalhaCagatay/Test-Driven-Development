5f4911fb707a31a2ab5cafb75095c8d45535a832
Talha Çağatay IŞIK <talhacagatayisik@gmail.com>
2018-08-20 16:00:49 +0300

    TDD
    This is a tdd applied on an existing polymorphism project.

---
+++ TDDWithPolymorphism/Polymorphism/Program.cs
@@ -0,0 +1,70 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+
+namespace Polymorphism
+{
+    public class Employee
+    {
+        public virtual string CalculateWeeklySalary(int weeklyHours, int wage)
+        {
+            var salary = 40 * wage;
+            string result = $"This ANGRY EMPLOYEE worked {weeklyHours} hrs. " +
+                            $"Paid for 40 hrs at $ {wage}" +
+                            $"/hr = ${salary}";
+            Console.WriteLine("\n" + result + " \n");
+            Console.WriteLine("---------------------------------------------\n");
+            return result;
+        }
+    }
+
+    public class Contractor : Employee
+    {
+        public override string CalculateWeeklySalary(int weeklyHours, int wage)
+        {
+            var salary = weeklyHours * wage;
+            string result = $"This HAPPY CONTRACTOR worked {weeklyHours} hrs. " +
+                            $"Paid for {weeklyHours} hrs at $ {wage}" +
+                            $"/hr = ${salary} ";
+            Console.WriteLine("\n" + result + " \n");
+            return result;
+        }
+    }
+
+
+
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            const int hours = 55, wage = 70;
+            //List<Employee> employees = Utils.GetEmployees();
+
+            var mock = new Mock<Utils>();
+            mock.Setup(m => m.GetMockEmployees()).Returns(() =>
+             new List<Employee> { new Contractor(), new Employee() });
+
+            List<Employee> employees = mock.Object.GetMockEmployees();
+
+            foreach (var e in employees)
+            {
+                e.CalculateWeeklySalary(hours, wage);
+            }
+        }        
+    }
+
+    public class Utils {
+        //public static List<Employee> GetEmployees()
+        //{
+        //    var someEmployee = new Employee();
+        //    var someContractor = new Contractor();
+        //    var everyone = new List<Employee> { someEmployee, someContractor };
+        //    return everyone;
+        //}
+
+        public virtual List<Employee> GetMockEmployees()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TDDWithPolymorphism/PolymorphismTestProject/UnitTest1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Polymorphism;
+
+namespace PolymorphismTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CalculateWeeklySalaryForEmployeeTest_70wage55hoursReturns2800Dollars()
+        {
+            // Arrange  
+            int weeklyHours = 55;
+            int wage = 70;
+            int salary = 40 * wage;
+            Employee e = new Employee();
+            
+            string expectedResponse = String.Format("This ANGRY EMPLOYEE worked {0} hrs. " +
+                            "Paid for 40 hrs at $ {1}" +
+                            "/hr = ${2}", weeklyHours, wage, salary);
+
+            // Act   
+            string response = e.CalculateWeeklySalary(weeklyHours, wage);
+
+            // Assert
+            Assert.AreEqual(response, expectedResponse);
+        }
+
+        [TestMethod]
+        public void CalculateWeeklySalaryForContractorTest_70wage55hoursReturns3850Dollars()
+        {
+            // Arrange  
+            int weeklyHours = 55;
+            int wage = 70;
+            int salary = weeklyHours * wage;
+            Contractor e = new Contractor();
+
+            string expectedResponse = String.Format("This HAPPY CONTRACTOR worked {0} hrs. " +
+                            "Paid for {0} hrs at $ {1}" +
+                            "/hr = ${2} ", weeklyHours, wage, salary);
+
+            // Act   
+            string response = e.CalculateWeeklySalary(weeklyHours, wage);
+
+            // Assert
+            Assert.AreEqual(response, expectedResponse);
+        }
+
+        [TestMethod]
+        public void CalculateWeeklySalaryForEmployeeTest_70wage55hoursDoesNotReturnCorrectString()
+        {
+            // Arrange  
+            int weeklyHours = 55;
+            int wage = 70;
+            int salary = 40 * wage;
+            Employee e = new Employee();
+
+            string expectedResponse = String.Format("Problem 1-This ANGRY EMPLOYEE worked {0} hrs. " +
+                            "Paid for 40 hrs at $ {1}" +
+                            "/hr = ${2}", weeklyHours, wage, salary);
+
+            // Act   
+            string response = e.CalculateWeeklySalary(weeklyHours, wage);
+
+            // Assert
+            Assert.AreNotEqual(response, expectedResponse);
+        }
+
+        [TestMethod]
+        public void CalculateWeeklySalaryForContractorTest_70wage55hoursDoesNotReturnCorrectString()
+        {
+            // Arrange  
+            int weeklyHours = 55;
+            int wage = 70;
+            int salary = weeklyHours * wage;
+            Contractor e = new Contractor();
+
+            string expectedResponse = String.Format("Problem 2-This HAPPY CONTRACTOR worked {0} hrs. " +
+                            "Paid for {0} hrs at $ {1}" +
+                            "/hr = ${2} ", weeklyHours, wage, salary);
+
+            // Act   
+            string response = e.CalculateWeeklySalary(weeklyHours, wage);
+
+            // Assert
+            Assert.AreNotEqual(response, expectedResponse);
+        }
+    }
+}
